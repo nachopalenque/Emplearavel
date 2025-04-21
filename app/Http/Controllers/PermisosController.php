b4b545cc8ff9080804cf4c3356be90dd6a2e025b
nacho <nxplktr930127@proton.me>
2025-04-21 16:45:13 +0000

    - Implementada validación que comprueba que no haya ningún rol o permiso creado previamente antes de crear los roles y permisos iniciales, los cuales se crearan al crear el primer centro productivo

--- app/Http/Controllers/PermisosController.php
+++ app/Http/Controllers/PermisosController.php
@@ -10,13 +10,27 @@ class PermisosController extends Controller
 {
     public static function plantillaRolesPermisos(){
 
-        $role = Role::create(['name' => 'Administrador']);
-        $role = Role::create(['name' => 'ProductManager']);
-        $role = Role::create(['name' => 'Usuario']);
-        $permiso = Permission::create(['name' => 'Crear']);
-        $permiso = Permission::create(['name' => 'Editar']);
-        $permiso = Permission::create(['name' => 'Ver']);
-        $permiso = Permission::create(['name' => 'Eliminar']);
+        //Me obtengo tanto los roles como los permisos
+        $roles = Role::all();
+        $permisos = Permission::all();
+
+        //Si no existe ningún rol creo la plantilla inicial de roles
+        if(count($roles) == 0){
+            $role = Role::create(['name' => 'Administrador']);
+            $role = Role::create(['name' => 'ProductManager']);
+            $role = Role::create(['name' => 'Usuario']);
+        }
+
+        //Si no existe ningún permiso creo la plantilla inicial de permisos
+        if(count($permisos) == 0){
+        
+            $permiso = Permission::create(['name' => 'Crear']);
+            $permiso = Permission::create(['name' => 'Editar']);
+            $permiso = Permission::create(['name' => 'Ver']);
+            $permiso = Permission::create(['name' => 'Eliminar']);
+        }       
+
+     
 
     }
 }
